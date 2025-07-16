4889294c0c6e9c7c089aefbb63a301f96a311827
May <92554133+qtmei@users.noreply.github.com>
2025-07-16 14:38:49 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,9 @@ int main()
 {
 	cout << "© 2021 Meiware.net" << endl << "simulates key press (no memory modification)" << endl << "hold mouse5 to autostrafe" << endl;
 
+	const WORD VK_A = MapVirtualKey((WORD)'A', MAPVK_VK_TO_VSC);
+	const WORD VK_D = MapVirtualKey((WORD)'D', MAPVK_VK_TO_VSC);
+
 	bool runonce = true;
 	RECT rect;
 	POINT point;
@@ -16,29 +19,28 @@ int main()
 	while(true)
 	{
 		GetWindowRect(GetForegroundWindow(), &rect);
+		GetCursorPos(&point);
 
 		if(GetAsyncKeyState(VK_XBUTTON2))
 		{
-			GetCursorPos(&point);
-
 			if(point.x < (rect.right / 2))
 			{
 				input.ki.dwFlags = KEYEVENTF_KEYUP;
-				input.ki.wVk = 'D';
+				input.ki.wScan = VK_D;
 				SendInput(1, &input, sizeof(INPUT));
 
 				input.ki.dwFlags = 0;
-				input.ki.wVk = 'A';
+				input.ki.wScan = VK_A;
 				SendInput(1, &input, sizeof(INPUT));
 			}
 			else if(point.x > (rect.right / 2))
 			{
 				input.ki.dwFlags = KEYEVENTF_KEYUP;
-				input.ki.wVk = 'A';
+				input.ki.wScan = VK_A;
 				SendInput(1, &input, sizeof(INPUT));
 
 				input.ki.dwFlags = 0;
-				input.ki.wVk = 'D';
+				input.ki.wScan = VK_D;
 				SendInput(1, &input, sizeof(INPUT));
 			}
 
@@ -49,10 +51,10 @@ int main()
 			{
 				input.ki.dwFlags = KEYEVENTF_KEYUP;
 
-				input.ki.wVk = 'A';
+				input.ki.wScan = VK_A;
 				SendInput(1, &input, sizeof(INPUT));
 
-				input.ki.wVk = 'D';
+				input.ki.wScan = VK_D;
 				SendInput(1, &input, sizeof(INPUT));
 
 				runonce = false;
