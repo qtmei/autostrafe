b2043cc0513ced291bfaa312f6d567caaa61144a
May <92554133+qtmei@users.noreply.github.com>
2025-07-16 17:35:50 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,8 +7,8 @@ int main()
 {
 	cout << "© 2021 Meiware.net" << endl << "simulates key press (no memory modification)" << endl << "hold mouse5 to autostrafe" << endl;
 
-	const WORD VK_A = MapVirtualKey((WORD)'A', MAPVK_VK_TO_VSC);
-	const WORD VK_D = MapVirtualKey((WORD)'D', MAPVK_VK_TO_VSC);
+	const WORD VK_A = MapVirtualKey((WORD)'A', MAPVK_VK_TO_VSC); //maps hardware key to virtual key so we emulate low level and are multi-device compatible
+	const WORD VK_D = MapVirtualKey((WORD)'D', MAPVK_VK_TO_VSC); //using a constant variable because devices aren't changed frequently and won't happen during runtime
 
 	bool runonce = true;
 	RECT rect;
@@ -21,7 +21,7 @@ int main()
 		GetWindowRect(GetForegroundWindow(), &rect);
 		GetCursorPos(&point);
 
-		if(GetAsyncKeyState(VK_XBUTTON2))
+		if(GetAsyncKeyState(VK_XBUTTON2)) //XBUTTON2 is the virtual key code for mouse5
 		{
 			if(point.x < (rect.right / 2))
 			{
@@ -29,7 +29,7 @@ int main()
 				input.ki.wScan = VK_D;
 				SendInput(1, &input, sizeof(INPUT));
 
-				input.ki.dwFlags = 0;
+				input.ki.dwFlags = 0; //0 defines a keydown event
 				input.ki.wScan = VK_A;
 				SendInput(1, &input, sizeof(INPUT));
 			}
@@ -47,7 +47,7 @@ int main()
 			runonce = true;
 		}
 		else
-			if(runonce)
+			if(runonce) //we release the keys when mouse5 is not held, but only once
 			{
 				input.ki.dwFlags = KEYEVENTF_KEYUP;
 
@@ -60,7 +60,7 @@ int main()
 				runonce = false;
 			}
 
-		Sleep(5);
+		Sleep(1);
 	}
 
 	return 0;
