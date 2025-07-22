858feb4944b39aa032f05e120950a5764a11f0df
May <92554133+qtmei@users.noreply.github.com>
2025-07-21 23:08:56 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -12,30 +12,29 @@ void SimulateKey(INPUT &input, WORD VK_KEY, bool pressed)
 
 int main()
 {
-	cout << "© 2021 Meiware.net" << endl << "simulates key press (no memory modification)" << endl << "hold mouse5 to autostrafe" << endl;
-
 	const WORD VK_A = MapVirtualKey((WORD)'A', MAPVK_VK_TO_VSC); //maps hardware key to virtual key so we emulate low level and are multi-device compatible
 	const WORD VK_D = MapVirtualKey((WORD)'D', MAPVK_VK_TO_VSC); //using a constant variable because devices aren't changed frequently and won't happen during runtime
-
 	bool runonce = true;
-	RECT rect;
-	POINT point;
+	RECT screen;
+	POINT cursor;
 	INPUT input;
 	input.type = INPUT_KEYBOARD;
 
+	cout << "© 2021 Meiware.net\nsimulates key press (no memory modification)\nhold mouse5 to autostrafe" << endl;
+
 	while(true)
 	{
-		GetWindowRect(GetForegroundWindow(), &rect);
-		GetCursorPos(&point);
+		GetWindowRect(GetForegroundWindow(), &screen);
+		GetCursorPos(&cursor);
 
 		if(GetAsyncKeyState(VK_XBUTTON2)) //XBUTTON2 is the virtual key code for mouse5
 		{
-			if(point.x < (rect.right / 2))
+			if(cursor.x < (screen.right / 2))
 			{
 				SimulateKey(input, VK_D, false);
 				SimulateKey(input, VK_A, true);
 			}
-			else if(point.x > (rect.right / 2))
+			else if(cursor.x > (screen.right / 2))
 			{
 				SimulateKey(input, VK_A, false);
 				SimulateKey(input, VK_D, true);
