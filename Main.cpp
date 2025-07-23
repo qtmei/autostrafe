0a58012f6c38e67e1d550c67d79c4941f1b715c9
May <92554133+qtmei@users.noreply.github.com>
2025-07-22 23:58:38 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,18 +3,20 @@
 
 using namespace std;
 
-void SimulateKey(INPUT &input, WORD VK_KEY, bool pressed)
+void SimulateKey(INPUT &input, WORD key, WORD vKey, bool pressed)
 {
 	input.ki.dwFlags = pressed ? 0 : KEYEVENTF_KEYUP;
-	input.ki.wScan = VK_KEY;
+	input.ki.wScan = vKey; //low level key simulation
+	input.ki.wVk = key; //high level key simulation, for some programs this is necessary
 	SendInput(1, &input, sizeof(INPUT));
 }
 
 int main()
 {
-	const WORD VK_A = MapVirtualKey((WORD)'A', MAPVK_VK_TO_VSC); //maps hardware key to virtual key so we emulate low level and are multi-device compatible
-	const WORD VK_D = MapVirtualKey((WORD)'D', MAPVK_VK_TO_VSC); //using a constant variable because devices aren't changed frequently and won't happen during runtime
+	const WORD K_A = (WORD)'A', K_D = (WORD)'D';
+	const WORD VK_A = MapVirtualKey(K_A, MAPVK_VK_TO_VSC), VK_D = MapVirtualKey(K_D, MAPVK_VK_TO_VSC); //maps hardware key to virtual key so we emulate low level and are multi-device compatible
 	bool runonce = true;
+
 	RECT screen;
 	POINT cursor;
 	INPUT input;
@@ -31,13 +33,13 @@ int main()
 		{
 			if(cursor.x < (screen.right / 2))
 			{
-				SimulateKey(input, VK_D, false);
-				SimulateKey(input, VK_A, true);
+				SimulateKey(input, K_D, VK_D, false);
+				SimulateKey(input, K_A, VK_A, true);
 			}
 			else if(cursor.x > (screen.right / 2))
 			{
-				SimulateKey(input, VK_A, false);
-				SimulateKey(input, VK_D, true);
+				SimulateKey(input, K_A, VK_A, false);
+				SimulateKey(input, K_D, VK_D, true);
 			}
 
 			runonce = true;
@@ -45,8 +47,8 @@ int main()
 		else
 			if(runonce) //we release the keys when mouse5 is not held, but only once
 			{
-				SimulateKey(input, VK_A, false);
-				SimulateKey(input, VK_D, false);
+				SimulateKey(input, K_A, VK_A, false);
+				SimulateKey(input, K_D, VK_D, false);
 
 				runonce = false;
 			}
